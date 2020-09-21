77ea9b612998630ed38d1591c3a30900260f0a69
Sonya Korolev Shapiro <sonyakorolevshapiro@Sonyas-MacBook-Air.local>
2020-09-21 14:33:54 -0400

    add tests

---
+++ asyncBreeds.js
@@ -0,0 +1,17 @@
+const fs = require('fs');
+
+const breedDetailsFromFile = function(breed, cb) {
+  console.log('breedDetailsFromFile: Calling readFile...');
+  fs.readFile(`./data/${breed}.txt`, 'utf8', (error, data) => {
+    
+    console.log('Callback: I have the data!');
+    if (!error) {
+      cb(data);
+    } else {
+      cb(undefined);
+    }
+  });
+};
+
+
+module.exports = breedDetailsFromFile;
